068b61e9cf88aa0d3a56d1ae5ba703f8829c093e
Mahmoud Alwakeel <alwakeel@Mahmouds-MacBook-Pro.local>
2023-11-03 15:05:09 +0200

    added class component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,39 @@
-import logo from './logo.svg';
+import { render } from '@testing-library/react';
 import './App.css';
+import Greet from './components/Greet'
+import Welcome from './components/Welcome';
+import { Component } from 'react';
 
+// moved this function to components/Greet.js
+// function Greet() {
+//   return (
+//     <h1>Hellooo there</h1>
+//   )
+//}
 
-function Header() {
-  return (
-    <h1>Hellooo there</h1>
-  )
-}
-function App() {
-  return <Header />;
+// changed App from a function component to a class component
+class App extends Component {
+  render() {
+    return(
+      <div className="App">
+      <Greet />,
+      <Welcome />
+      </div>
+    )
+  }
+
 }
 
 export default App;
+
+
+/*
+render() {
+    return (
+      <div className='App'>
+      <Greet />,
+      <Welcome />
+      </div>
+    );
+  }
+*/
---
+++ src/components/Greet.js
@@ -0,0 +1,15 @@
+import React from "react";
+// this is a functional component 
+
+
+// function Greet() {
+//     return (
+//         <h1>Hello there</h1>
+//     )
+// }
+
+
+/* export */ const Greet = () => <h1>Hello there from a functional component</h1>
+
+// for most of the cases we will be used default export
+export default Greet
